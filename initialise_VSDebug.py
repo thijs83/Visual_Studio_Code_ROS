e581f77440429c2c31f3c096df1279067f14a479
thijs83 <thijs_niesten@hotmail.com>
2022-09-27 20:11:13 +0200

    files and scripts

---
+++ initialise_VSDebug.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import os
+import sys
+
+# Retrieve the workspace folder
+workspace_folder = str(Path().absolute())
+
+########################################################################################
+## Creation of the tasks.json file
+########################################################################################
+tasks_data = {
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "ROS: central_catkin_make",
+            "type": "catkin_make",
+            "args": [
+                "--directory",
+				workspace_folder,
+                "-j4",
+                "-DCMAKE_BUILD_TYPE=Debug",
+                "-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
+            ],
+            "problemMatcher": "$catkin-gcc",
+            "group": {
+                "kind": "build",
+                "isDefault": True
+            }
+        },
+        {
+            "label": "ROS: update Build & Debug",
+            "command": "python3 /home/thijs/vs_test3/update_VSDebug.py",
+            "type": "shell",
+            "group": {
+                "kind": "build",
+                "isDefault": True
+            },
+            "presentation": {
+                "reveal": "always",
+                "panel": "new",
+                "focus": True
+            },"dependsOn": [
+                "ROS: central_catkin_make"                               
+            ]
+        }
+    ]
+}
+# Store the data in the json file
+with open('.vscode/tasks.json', 'w') as jsonFile_tasks:
+    json.dump(tasks_data, jsonFile_tasks, indent=4)
+    jsonFile_tasks.close()
+
+########################################################################################
+## Creation of the c_cpp_properties.json file
+########################################################################################
+c_cpp_properties_data = {
+    "configurations": [
+        {
+            "name": "Linux",
+            "intelliSenseMode": "gcc-x64",
+            "compilerPath": "/usr/bin/g++",
+            "cStandard": "c11",
+            "cppStandard": "c++17",
+            "compileCommands": "${workspaceFolder}/build/compile_commands.json"
+        }
+    ],
+    "version": 4
+}
+# Store the data in the json file
+with open('.vscode/c_cpp_properties.json', 'w') as jsonFile_c_cpp_properties:
+    json.dump(c_cpp_properties_data, jsonFile_c_cpp_properties, indent=4)
+    jsonFile_c_cpp_properties.close()
+
+########################################################################################
+## Creation of the extensions.json file
+########################################################################################
+extensions_data = {
+	"recommendations": [
+		"eamodio.gitlens",
+        "ms-iot.vscode-ros",
+        "ms-python.python",
+        "ms-vscode.cpptools",
+        "twxs.cmake",
+        "vscode-icons-team.vscode-icons"
+	]
+}
+# Store the data in the json file
+with open('.vscode/extensions.json', 'w') as jsonFile_extensions:
+    json.dump(extensions_data, jsonFile_extensions, indent=4)
+    jsonFile_extensions.close()
+
+########################################################################################
+## Creation of the settings.json file
+########################################################################################
+settings_data = {
+    "python.autoComplete.extraPaths": [
+        "/opt/ros/noetic/lib/python3/dist-packages"
+    ],
+    "cmake.sourceDirectory": "${workspaceFolder}/src",
+    "cmake.configureOnOpen": False
+}
+# Store the data in the json file
+with open('.vscode/settings.json', 'w') as jsonFile_settings:
+    json.dump(settings_data, jsonFile_settings, indent=4)
+    jsonFile_settings.close()
---
+++ update_VSDebug.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import os
+import sys
+import glob
+
+# Retrieve the workspace folder
+workspace_folder = str(Path().absolute())
+
+# Find all packages and check if there exists atleast one
+package_names = [f for f in os.listdir(workspace_folder+"/src") if os.path.isdir(os.path.join(workspace_folder+"/src",f))]
+
+if not package_names:
+    print("There are no packages and thus nothing is build")
+    sys.exit(1)
+
+# Check if initial build is done
+try:
+    test = os.listdir(workspace_folder+"/devel")
+
+    if not test:
+        print("First build the envirnment, before calling this script")
+        sys.exit(1)
+except:
+    print("First build the envirnment, before calling this script")
+    sys.exit(1)
+
+########################################################################################
+## Creation of the launch.json file
+########################################################################################
+launch_data_array = []
+for i in package_names:
+    # Find all executables inside package
+    executable_names = [f for f in os.listdir(workspace_folder+"/devel/lib/"+str(i)) if os.path.isfile(os.path.join(workspace_folder+"/devel/lib/"+str(i),f))]
+
+    # Remove name cmake.lock
+    if "cmake.lock" in executable_names:
+        executable_names.remove("cmake.lock")
+    
+    # CPP and Python file name in their own array
+    python_executable_names = [name for name in executable_names if name.endswith(".py")]
+    cpp_executable_names = [name for name in executable_names if not name.endswith(".py")]
+
+    # Add the CPP files to the launch file
+    for j in cpp_executable_names:     
+        launch_data_array.append({
+                        "name": "c++: "+str(j)+"_Node - PKG:"+str(i),
+                        "type": "cppdbg",
+                        "request": "launch",
+                        "program": "${workspaceFolder}/devel/lib/"+str(i)+"/"+str(j),
+                        "args": [],
+                        "stopAtEntry": False,
+                        "cwd": "${workspaceFolder}/../../",
+                        "environment": [],
+                        "externalConsole": False,
+                        "MIMode": "gdb",
+                        "setupCommands": [
+                            {
+                                "description": "Enable pretty-printing for gdb",
+                                "text": "-enable-pretty-printing",
+                                "ignoreFailures": True
+                            }
+                        ]
+                    })
+
+    # Find the python files and also add them to the launch file
+    for j in python_executable_names:
+        path_package = "./src/"+str(i)
+        path_file = glob.glob(path_package + "/**/"+str(j), recursive = True)
+        
+        path = path_file[0]
+
+        launch_data_array.append({
+                        "name": "py: "+str(j)+"_Node - PKG:"+str(i),
+                        "type": "python",
+                        "request": "launch",
+                        "program": "${workspaceFolder}"+path[1:]
+        })
+
+# Give the correct form to the launch file     
+launch_data = {}
+launch_data["configurations"] = launch_data_array
+
+# Store the data in the json file
+with open('.vscode/launch.json', 'w') as jsonFile_launch:
+    json.dump(launch_data, jsonFile_launch, indent=4)
+    jsonFile_launch.close()
+
